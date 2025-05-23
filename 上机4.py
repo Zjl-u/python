7f3410fc9c17befa4f8c29e31b62e2e12660139c
Zjl-u <2373007562@qq.com>
2025-05-23 23:11:55 +0800

    Add files via upload

---
+++ 上机4.py
@@ -0,0 +1,15 @@
+import turtle
+def main():
+    turtle.hideturtle()
+    square(100,0,50,'blue')
+def square(x, y, width, color):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.fillcolor(color)
+    turtle.pendown()
+    turtle.begin_fill()
+    for count in range(4):
+        turtle.forward(width)
+        turtle.left(90)
+    turtle.end_fill()
+main()
